ed7393c2f029c2c4d7921a52eea85e6d90e1d7d0
Fin Mavis <fin.mavis@yahoo.com>
2019-03-31 15:05:51 +0700

    Delete common configuration and add some optimise for production

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,8 +1,17 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
-module.exports = merge(common, {
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].[chunkhash].js',
+  },
   mode: 'development',
   devServer: {
     contentBase: path.join(__dirname, '../dist'),
@@ -11,4 +20,68 @@ module.exports = merge(common, {
     overlay: true,
   },
   devtool: 'inline-source-map',
-});
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(sa|sc)ss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: ['img:src', 'link:href', ':data-src'],
+            minimize: true,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {}),
+    // The plugin will generate an HTML5 file for you that includes all your webpack bundles in the body using script tags
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new WebpackMd5Hash(),
+    new CompressionPlugin({
+      algorithm: 'gzip',
+    }),
+  ],
+};
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,93 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = merge(common, {
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'main.bundle.js',
+  },
   mode: 'production',
   devtool: 'source-map',
-});
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(sa|sc)ss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: ['img:src', 'link:href', ':data-src'],
+            minimize: true,
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({}),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {}),
+    new MiniCssExtractPlugin({
+      filename: 'style.min.css',
+    }),
+    // The plugin will generate an HTML5 file for you that includes all your webpack bundles in the body using script tags
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new CompressionPlugin({
+      algorithm: 'gzip',
+    })
+  ],
+};
