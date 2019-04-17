6074b03001a367cad3f057d05d52e15415c570b1
Fin Mavis <fin.mavis@yahoo.com>
2019-04-17 11:14:46 +0700

    Add example using async await

--- src/importme.js
+++ src/importme.js
@@ -4,3 +4,9 @@ document.querySelector('.testing').src = Image;
 console.log('Console.log from importme.js');
 
 export const hello = hello => console.log('Import me Please!');
+
+export const tes = function() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Some Data"), 1000);
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import { hello } from './importme';
+import { hello, tes } from './importme';
 
 import './style.css';
 import './style.scss';
 import './style.sass';
 
 hello();
+
+async function run() {
+  const value = await tes();
+  console.log(value)
+}
+
+run();
